45b077c5fb386d19e144a44703088c0895ea9381
Ferry Slot <ferryslot@hotmail.com>
2018-03-20 16:07:12 +0100

    readme

--- opdracht3/v2/script.js
+++ opdracht3/v2/script.js
@@ -2,22 +2,69 @@
 
 var requestCryptoURL = 'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,XRP,LTC,EOS,ETC,BCH,QTUM,NEO,TRX&tsyms=EUR,USD';
 var requestCrypto = new XMLHttpRequest();
-
 requestCrypto.open('GET', requestCryptoURL);
 requestCrypto.responseType = 'json';
 requestCrypto.send();
+var isJSONloaded = 0;
+var selectCrypto = document.getElementById('selectcrypto');
+var cryptoResponse;
 
 requestCrypto.onload = function() 
 {
-    var cryptoResponse = requestCrypto.response;
+    isJSONloaded = 1;
+    cryptoResponse = requestCrypto.response;
     //console.log("Crypto respones: " + cryptoResponse);
-    showCrypto(cryptoResponse['DISPLAY'].XRP.EUR);
-    showCrypto(cryptoResponse['DISPLAY'].BTC.EUR);
+    //showCrypto(cryptoResponse['DISPLAY'].BTC.EUR);
 }
 
+selectCrypto.onchange = function()
+{
+    if(isJSONloaded == 1)
+    {
+        console.log(selectCrypto.value);
+        switch(selectCrypto.value) {
+        case "BTC":
+            showCrypto(cryptoResponse['DISPLAY'].BTC.EUR);
+        break;
+        case "ETH":
+            showCrypto(cryptoResponse['DISPLAY'].ETH.EUR);
+        break;
+        case "XRP":
+            showCrypto(cryptoResponse['DISPLAY'].XRP.EUR);
+        break;
+        case "LTC":
+            showCrypto(cryptoResponse['DISPLAY'].LTC.EUR);
+        break;
+        case "EOS":
+            showCrypto(cryptoResponse['DISPLAY'].EOS.EUR);
+        break;
+        case "ETC":
+            showCrypto(cryptoResponse['DISPLAY'].ETC.EUR);
+        break;
+        case "BCH":
+            showCrypto(cryptoResponse['DISPLAY'].BCH.EUR);
+        break;
+        case "QTUM":
+            showCrypto(cryptoResponse['DISPLAY'].QTUM.EUR);
+        break;
+        case "NEO":
+            showCrypto(cryptoResponse['DISPLAY'].NEO.EUR);
+        break;
+        case "TRX":
+            showCrypto(cryptoResponse['DISPLAY'].TRX.EUR);
+        break;
+        }
+    }
+}
 
 function showCrypto(jsonObj) 
 {
+    //remove everything
+    var resetcryptodiv = document.getElementById("cryptodiv");
+    while (resetcryptodiv.hasChildNodes()) {
+        resetcryptodiv.removeChild(resetcryptodiv.firstChild);
+    }
+    //build everything
     var cryptoarray = jsonObj;
     console.log(".size array: " + Object.keys(cryptoarray).length);
 
